0b0ec93f8cb398e31e2d13992aeced221798f334
Anton de Winter <anton@thelevelup.com>
2017-01-26 08:41:40 -0500

    lots of console out

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
 
-server.listen(8080);
+server.listen((process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+// app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+var echoFunc = function(req, res) {
+  var echo = "PARAMS: " + req.params.toString() + "\n";
+  echo += "HEADERS: " + req.headers.toString() + "\n";
+  echo += "BODY: " + req.body.toString() + "\n";
+  console.log("GOT REQUEST: ", req.method);
+  console.log("GOT BODY");
+  console.log(req.body);
+  console.log("FROM IP:", req.ip);
+  console.log("PARAMS", req.params);
+  console.log("HEADERS:", req.headers);
+  res.send(echo);
+}
+app.get('/', echoFunc);
+app.post('/', echoFunc);
+
+
 
 var i = 0.0;
 io.on('connection', function (socket) {
